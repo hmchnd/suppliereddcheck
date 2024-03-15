34a8b26b42c4f74c4b8c3074dadb20312f905232
Shobhit Tyagi <shobhittyagimit@gmail.com>
2024-03-15 12:13:23 +0000

    commit

--- webapp/controller/ListView.controller.js
+++ webapp/controller/ListView.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Fragment) {
         "use strict";
 
         return Controller.extend("com.sap.suppliereddcheck.controller.ListView", {
@@ -17,6 +18,31 @@ sap.ui.define([
             onCreate:function(){
                 this.getOwnerComponent().getRouter().navTo("RouteEddView");
 
-            }
+            },
+            onCreateVendor:function(){
+                this.getOwnerComponent().getRouter().navTo("RouteVendorView");
+            },
+            
+    onCreateNew: function(){
+    if (!this._CreateDialog) {
+                        Fragment.load({
+                            id: this.getView().getId(),
+                            name: "com.sap.suppliereddcheck.view.fragment.Create",
+                            controller: this
+                        }).then(oDialog => {
+                            this._CreateDialog = oDialog;
+                            this.getView().addDependent()
+                            oDialog.open()
+                        })
+                    }
+                    else {
+                        this._CreateDialog.open()
+                    }
+    
+                },
+                onCancel: function () {
+                    this._CreateDialog.close()
+    
+                },
         });
     });
